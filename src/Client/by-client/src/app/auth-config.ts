0ecf93076e6d8bc059967945f175ac453c1a4117
Brian Adams <schlechtums@gmail.com>
2024-04-08 20:05:44 -0400

    Angular 14 - from MSAL example

---
+++ src/Client/by-client/src/app/auth-config.ts
@@ -0,0 +1,91 @@
+/**
+ * This file contains authentication parameters. Contents of this file
+ * is roughly the same across other MSAL.js libraries. These parameters
+ * are used to initialize Angular and MSAL Angular configurations in
+ * in app.module.ts file.
+ */
+
+import { LogLevel, Configuration, BrowserCacheLocation } from '@azure/msal-browser';
+
+const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigator.userAgent.indexOf("Trident/") > -1;
+
+/**
+ * Enter here the user flows and custom policies for your B2C application,
+ * To learn more about user flows, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-overview
+ * To learn more about custom policies, visit https://docs.microsoft.com/en-us/azure/active-directory-b2c/custom-policy-overview
+ */
+export const b2cPolicies = {
+    names: {
+        signUpSignIn: 'b2c_1_susi',
+        resetPassword: 'B2C_1_reset_v3',
+        editProfile: 'B2C_1_edit_profile_v2',
+    },
+    authorities: {
+        signUpSignIn: {
+            authority: 'https://blunderyears.b2clogin.com/blunderyears.onmicrosoft.com/b2c_1_susi',
+        },
+        resetPassword: {
+            authority: 'https://blunderyears.b2clogin.com/blunderyears.onmicrosoft.com/B2C_1_reset_v3',
+        },
+        editProfile: {
+            authority: 'https://blunderyears.b2clogin.com/blunderyears.onmicrosoft.com/b2c_1_edit_profile_v2',
+        },
+    },
+    authorityDomain: 'blunderyears.b2clogin.com',
+};
+
+/**
+ * Configuration object to be passed to MSAL instance on creation.
+ * For a full list of MSAL.js configuration parameters, visit:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/configuration.md
+ */
+export const msalConfig: Configuration = {
+    auth: {
+        clientId: '634afe97-d457-4910-890f-a32d97c28d3f', // This is the ONLY mandatory field that you need to supply.
+        authority: b2cPolicies.authorities.signUpSignIn.authority, // Defaults to "https://login.microsoftonline.com/common"
+        knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
+        redirectUri: '/auth', // Points to window.location.origin by default. You must register this URI on Azure portal/App Registration.
+        postLogoutRedirectUri: '/', // Points to window.location.origin by default.
+    },
+    cache: {
+        cacheLocation: BrowserCacheLocation.LocalStorage, // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
+        storeAuthStateInCookie: isIE, // Set this to "true" if you are having issues on IE11 or Edge. Remove this line to use Angular Universal
+    },
+    system: {
+        /**
+         * Below you can configure MSAL.js logs. For more information, visit:
+         * https://docs.microsoft.com/azure/active-directory/develop/msal-logging-js
+         */
+        loggerOptions: {
+            loggerCallback(logLevel: LogLevel, message: string) {
+                console.log(message);
+            },
+            logLevel: LogLevel.Verbose,
+            piiLoggingEnabled: false
+        }
+    }
+}
+
+/**
+ * Add here the endpoints and scopes when obtaining an access token for protected web APIs. For more information, see:
+ * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
+ */
+export const protectedResources = {
+    apiTodoList: {
+        endpoint: "https://localhost:44351/api/todolist",
+        scopes: {
+            read: ["https://blunderyears.onmicrosoft.com/907e344f-2ef0-4b0e-8af8-d670ec2b8ccd/Application.Access"],
+            write: ["https://blunderyears.onmicrosoft.com/907e344f-2ef0-4b0e-8af8-d670ec2b8ccd/Application.Access"]
+        }
+    }
+}
+
+/**
+* Scopes you add here will be prompted for user consent during sign-in.
+* By default, MSAL.js will add OIDC scopes (openid, profile, email) to any login request.
+* For more information about OIDC scopes, visit:
+* https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
+*/
+export const loginRequest = {
+    scopes: []
+};
