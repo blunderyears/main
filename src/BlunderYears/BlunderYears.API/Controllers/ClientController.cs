a85ce58ea52e5695d6ab244137738cf712fdb40c
schlechtums <schlechtums@gmail.com>
2024-04-08 22:47:17 -0400

    Feature/authentication and c sharp client (#3)
    * Angular 14 - from MSAL example
    
    * c# client can login etc
    
    * build angular in pipeline
    
    * troubleshooting get directories
    
    * System.Environment.GetEnvironmentVariable("FUNCTIONS_APPLICATION_DIRECTORY")
    
    * default to javascript if didn't get contentType

---
+++ src/BlunderYears/BlunderYears.API/Controllers/ClientController.cs
@@ -0,0 +1,72 @@
+﻿namespace BlunderYears.API.Controllers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.StaticFiles;
+    using Microsoft.Azure.Functions.Worker;
+    using Microsoft.Azure.Functions.Worker.Http;
+    using Microsoft.Net.Http.Headers;
+
+    public class ClientController
+    {
+        [Function("ZClient")]
+        public async Task<HttpResponseData> ZClient(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{*urlPath}")] HttpRequestData request,
+            string urlPath)
+        {
+            /*var contentFolder = Path.Combine(Directory.GetCurrentDirectory(), "angular");
+            var response2 = request.CreateResponse(System.Net.HttpStatusCode.OK);
+            response2.WriteAsJsonAsync(Directory.EnumerateDirectories(Directory.GetCurrentDirectory()).ToList());
+            return response2;*/
+
+            var contentFolder = Path.Combine(Environment.GetEnvironmentVariable("FUNCTIONS_APPLICATION_DIRECTORY"), "angular");
+
+            if (!string.IsNullOrWhiteSpace(urlPath))
+            {
+                var folder = contentFolder;
+                var pathSegments = urlPath.Split('/');
+                for (var i = 0; i < pathSegments.Length - 1; i++)
+                {
+                    var pathSegment = pathSegments[i];
+                    folder = Path.Combine(folder, pathSegment);
+                }
+
+                var fileSegment = pathSegments.Last();
+                var file = Path.Combine(folder, fileSegment);
+
+                if (!File.Exists(file))
+                {
+                    return await Index(contentFolder, request);
+                }
+
+                new FileExtensionContentTypeProvider().TryGetContentType(urlPath, out var contentType);
+
+                var response = request.CreateResponse(System.Net.HttpStatusCode.OK);
+                response.Headers.TryAddWithoutValidation("Content-Type", contentType ?? "text/javascript");
+                await response.WriteBytesAsync(await File.ReadAllBytesAsync(file));
+                return response;
+            }
+            else
+            {
+                return await Index(contentFolder, request);
+            }
+        }
+
+        private static async Task<HttpResponseData> Index(string contentFolder, HttpRequestData request)
+        {
+            var response = request.CreateResponse(System.Net.HttpStatusCode.OK);
+
+            // Prevent the caching of the index file as it could potentially link to old cached js files
+            response.Headers.TryAddWithoutValidation(HeaderNames.CacheControl, "no-cache, no-store, must-revalidate");
+            response.Headers.TryAddWithoutValidation(HeaderNames.Expires, "0");
+            response.Headers.TryAddWithoutValidation(HeaderNames.Pragma, "no-cache");
+
+            response.Headers.TryAddWithoutValidation("Content-Type", "text/html");
+
+            await response.WriteBytesAsync(await File.ReadAllBytesAsync(Path.Combine(contentFolder, "index.html")));
+            return response;
+        }
+    }
+}
--- src/BlunderYears/BlunderYears.API/Program.cs
+++ src/BlunderYears/BlunderYears.API/Program.cs
@@ -1,13 +1,24 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services =>
+    .ConfigureServices((ctx, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(
+            options =>
+            {
+                ctx.Configuration.Bind("AzureAdB2C", options);
+                options.Events = new JwtBearerEvents();
+            },
+            options => { ctx.Configuration.Bind("AzureAdB2C", options); });
     })
     .Build();
 
